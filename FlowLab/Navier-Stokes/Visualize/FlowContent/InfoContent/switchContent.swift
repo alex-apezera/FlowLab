ecc11a906b08981ea5f4858d252e95f909565778
alex-apezera <134931740+alex-apezera@users.noreply.github.com>
2026-09-06 15:03:29 +0300

    Update switchContent.swift

--- FlowLab/Navier-Stokes/Visualize/FlowContent/InfoContent/switchContent.swift
+++ FlowLab/Navier-Stokes/Visualize/FlowContent/InfoContent/switchContent.swift
@@ -15,59 +15,60 @@ extension Visualizator {
         let minimumScale: CGFloat = 0.5
         let maximumScale: CGFloat = 2.0
         
-        HStack {
-            
-            // Переключение объекта наблюдения
-            Picker("Switcher", selection: $selectedVisualization) {
-                ForEach(0..<visualizationOptions.count, id: \.self) { index in
-                    Text(visualizationOptions[index]).tag(index)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                
+                // Переключение объекта наблюдения
+                Picker("Switcher", selection: $selectedVisualization) {
+                    ForEach(0..<visualizationOptions.count, id: \.self) { index in
+                        Text(visualizationOptions[index]).tag(index)
+                    }
                 }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            // Переключатели режимов решения и просмотра (ВКЛ/ВЫКЛ)
-            
-            /// процесс плавления
-            Toggle(solver.params.allowMelt ? "𝗆❄️" : "𝗆💧", isOn: $solver.params.allowMelt).clipMode()
-                .keyboardShortcut("m", modifiers: [])
-            /// линия фронта плавления на тепловых картах
-            Toggle("🌗", isOn: $showFrontLine).clipMode()
-            /// наложение скоростей на тепловые карты
-            Toggle("+𝐕", isOn: $addVelocityField).clipMode()
-            /// цветовая схема тепловой карты
-            Toggle("🌈", isOn: $toggleColorScheme).clipMode()
-            /// цвет стрелок вектора скорости
-            Toggle("🚥 𝐕", isOn: $toggleVelocityColor).clipMode()
-            
-            // Увеличение/уменьшение плотности стрелок скоростей
-            Stepper("⇶⇉➔  \(arrowDensity)") {
-                if arrowDensity < 5 { arrowDensity += 1 }
-            } onDecrement: {
-                if arrowDensity > 1 { arrowDensity -= 1 }
-            }
-            .clipMode(180)
-            
-            // Масштабирование стрелок для скоростей
-            Stepper("⬆︎📶  \(arrowScale, specifier: "%.1f")") {
-                if arrowScale < 20 { arrowScale += 0.5 }
-            } onDecrement: {
-                if arrowScale > 0 { arrowScale -= 0.25 }
-            }
-            .clipMode(200)
-            
-            // Увеличение/уменьшение масштаба тепловой карты
-            Stepper("↕️  \(scale, specifier: "%.1f")") {
-                if scale <= maximumScale { withAnimation(.easeInOut(duration: 0.5)) { scale += step } }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                // Переключатели режимов решения и просмотра (ВКЛ/ВЫКЛ)
+                
+                /// процесс плавления
+                Toggle(solver.params.allowMelt ? "𝗆❄️" : "𝗆💧", isOn: $solver.params.allowMelt).clipMode()
+                    .keyboardShortcut("m", modifiers: [])
+                /// линия фронта плавления на тепловых картах
+                Toggle("🌗", isOn: $showFrontLine).clipMode()
+                /// наложение скоростей на тепловые карты
+                Toggle("+𝐕", isOn: $addVelocityField).clipMode()
+                /// цветовая схема тепловой карты
+                Toggle("🌈", isOn: $toggleColorScheme).clipMode()
+                /// цвет стрелок вектора скорости
+                Toggle("🚥 𝐕", isOn: $toggleVelocityColor).clipMode()
+                
+                // Увеличение/уменьшение плотности стрелок скоростей
+                Stepper("⇶⇉➔  \(arrowDensity)") {
+                    if arrowDensity < 5 { arrowDensity += 1 }
+                } onDecrement: {
+                    if arrowDensity > 1 { arrowDensity -= 1 }
+                }
+                .clipMode(180)
+                
+                // Масштабирование стрелок для скоростей
+                Stepper("⬆︎📶  \(arrowScale, specifier: "%.1f")") {
+                    if arrowScale < 20 { arrowScale += 0.5 }
+                } onDecrement: {
+                    if arrowScale > 0 { arrowScale -= 0.25 }
+                }
+                .clipMode(200)
+                
+                // Увеличение/уменьшение масштаба тепловой карты
+                Stepper("↕️  \(scale, specifier: "%.1f")") {
+                    if scale <= maximumScale { withAnimation(.easeInOut(duration: 0.5)) { scale += step } }
                 } onDecrement: {
                     if scale >= minimumScale { withAnimation(.easeInOut(duration: 0.5)) { scale -= step } }
+                }
+                .clipMode(180)
+                
+                // Сброс трансформаций тепловой карты
+                Button(action: resetTransformations) {
+                    Image(systemName: "arrow.2.circlepath.circle")
+                }
             }
-            .clipMode(180)
-            
-            // Сброс трансформаций тепловой карты
-            Button(action: resetTransformations) {
-                Image(systemName: "arrow.2.circlepath.circle")
-            }
-            
         }
         // Управление запуском/остановкой процесса плавления
         .onChange(of: solver.step) { _, newValue in
